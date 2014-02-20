9317a86b668cf72f860064658edd33cb5aec5edc
Yuan Zhou <myoilbox@gmail.com>
2014-02-20 18:20:08 +0800

    add logic to process several zip files

--- AMParser/main.m
+++ AMParser/main.m
@@ -8,43 +8,11 @@
 
 #import <Foundation/Foundation.h>
 #import "XRRun.h"
+#import "Utils.h"
 
 //#define templateUDID @"E3BF39BB-8DCB-4D12-BDC7-DC7D678EB5F3"
 #define templateUDID @"2DFBB7CF-2B32-41D6-9FA6-73CF813DFC24"
 
-@implementation NSString (TrimmingAdditions)
-
-- (NSString *)stringByTrimmingLeadingCharactersInSet:(NSCharacterSet *)characterSet {
-    NSUInteger location = 0;
-    NSUInteger length = [self length];
-    unichar charBuffer[length];
-    [self getCharacters:charBuffer];
-    
-    for (location; location < length; location++) {
-        if (![characterSet characterIsMember:charBuffer[location]]) {
-            break;
-        }
-    }
-    
-    return [self substringWithRange:NSMakeRange(location, length - location)];
-}
-
-- (NSString *)stringByTrimmingTrailingCharactersInSet:(NSCharacterSet *)characterSet {
-    NSUInteger location = 0;
-    NSUInteger length = [self length];
-    unichar charBuffer[length];
-    [self getCharacters:charBuffer];
-    
-    for (length; length > 0; length--) {
-        if (![characterSet characterIsMember:charBuffer[length - 1]]) {
-            break;
-        }
-    }
-    
-    return [self substringWithRange:NSMakeRange(location, length - location)];
-}
-
-@end
 
 int main(int argc, const char *argv[])
 {
@@ -60,53 +28,30 @@ int main(int argc, const char *argv[])
         NSString *workihngDic = [fileManager currentDirectoryPath];
         
         NSString *inputTraceFile = [[NSString stringWithUTF8String:argv[1]] stringByExpandingTildeInPath];
+        NSString *resultZipDirectory = [NSString stringWithFormat:@"%@/instrument_data/%@/run_data", inputTraceFile, templateUDID];
         
-        NSString *resultZipFile = [NSString stringWithFormat:@"%@/instrument_data/%@/run_data/1.run.zip", inputTraceFile, templateUDID];
+        NSError *error;
+        NSArray * directoryContents = [fileManager contentsOfDirectoryAtPath:resultZipDirectory error:&error];
         
-        if (![fileManager fileExistsAtPath:resultZipFile])
-        {
+        if (error) {
             printf("No Activity Manager trace result found!\n");
             exit(1);
         }
         
-        // Unzip file
-        NSTask *task;
-        task = [[NSTask alloc] init];
-        [task setLaunchPath: @"/usr/bin/unzip"];
-        
-        NSArray *arguments;
-        arguments = @[@"-o", resultZipFile];
-        [task setArguments: arguments];
-        
-        NSPipe *pipe;
-        pipe = [NSPipe pipe];
-        [task setStandardOutput: pipe];
-        
-        NSFileHandle *file;
-        file = [pipe fileHandleForReading];
-        
-        [task launch];
-        
-        NSData *data;
-        data = [file readDataToEndOfFile];
-        
-        NSString *string;
-        string = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
-        
-        NSRange startRange = [string rangeOfString:@"inflating: "];
-        NSString *unzipedFile = [[string substringFromIndex:(startRange.location + startRange.length)] stringByTrimmingTrailingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-        
-        NSString *resultUnzippedFile = [NSString stringWithFormat:@"%@/%@",workihngDic, unzipedFile];
-        
-        printf ("\nunzip trace file:\n%s\n", [string UTF8String]);
-        
-		// Read the trace file into memory
-		NSURL *traceFile = [NSURL fileURLWithPath:[resultUnzippedFile stringByExpandingTildeInPath]];
-		NSData *traceData = [NSData dataWithContentsOfURL:traceFile];
-		
-		// Deserialize the data and dump its content
-		XRActivityInstrumentRun *run = [NSUnarchiver unarchiveObjectWithData:traceData];
-		printf("\n%s\n", [[run description] UTF8String]);
+        for (NSString *zipFile in directoryContents) {
+            NSString *unzipedFile = [Utils unzipFile:[NSString stringWithFormat:@"%@/%@",resultZipDirectory, zipFile]];
+            if (![unzipedFile isEqualToString:ZIP_FAILED]) {
+                NSString *resultUnzippedFile = [NSString stringWithFormat:@"%@/%@",workihngDic, unzipedFile];
+                
+                // Read the trace file into memory
+                NSURL *traceFile = [NSURL fileURLWithPath:[resultUnzippedFile stringByExpandingTildeInPath]];
+                NSData *traceData = [NSData dataWithContentsOfURL:traceFile];
+                
+                // Deserialize the data and dump its content
+                XRActivityInstrumentRun *run = [NSUnarchiver unarchiveObjectWithData:traceData];
+                printf("\n%s\n", [[run description] UTF8String]);
+            }
+        }
 	}
 	
     return 0;
