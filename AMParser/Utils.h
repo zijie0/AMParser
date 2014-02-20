ae00fb349a99f0a606c0f324fd33e8153368968c
Yuan Zhou <myoilbox@gmail.com>
2014-02-20 18:18:58 +0800

    add logic to process several zip files

---
+++ AMParser/Utils.h
@@ -0,0 +1,13 @@
+//
+//  Utils.h
+//  AMParser
+//
+//  Created by Zhou, Yuan on 2/20/14.
+//  Copyright (c) 2014 Zhou, Yuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utils : NSObject
+
+@end
---
+++ AMParser/Utils.m
@@ -0,0 +1,13 @@
+//
+//  Utils.m
+//  AMParser
+//
+//  Created by Zhou, Yuan on 2/20/14.
+//  Copyright (c) 2014 Zhou, Yuan. All rights reserved.
+//
+
+#import "Utils.h"
+
+@implementation Utils
+
+@end
